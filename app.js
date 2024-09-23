dd93400fe53f03a6688168886e8ee9f0030c26bc
Israel Silva <israel@iasdigitalgroup.com>
2024-09-23 01:29:29 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,138 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      cols: [[5], [1, 2], [4], [1, 2], [5]],
+      rows: [[2, 2], [1, 1, 1], [1, 1, 1], [5], [5]],
+      board: [
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+      ],
+      boardSize: 5,
+      interactions: 0,
+    };
+  },
+  methods: {
+    setCell(row, col, value) {
+      if (this.board[row][col] == "") {
+        this.board[row][col] = value;
+      }
+    },
+    checkCols() {
+      for (let i = 0; i < this.cols.length; i++) {
+        if (this.cols[i].length == 1 && this.cols[i][0] == this.boardSize) {
+          for (let k = 0; k < this.boardSize; k++) {
+            this.setCell(k, i, "active");
+          }
+        }
+
+        let options =
+          this.boardSize -
+          this.cols[i].reduce((a, b) => a + b, 0) +
+          this.cols[i].length;
+        let tempCol = [];
+        for (let o = 0; o < options; o++) {
+          tempCol[o] = [];
+
+          for (let j = 0; j < o; j++) {
+            tempCol[o].push("clear");
+          }
+
+          for (let k = 0; k < this.cols[i].length; k++) {
+            for (let j = 0; j < this.cols[i][k]; j++) {
+              tempCol[o].push("active");
+            }
+            if (tempCol.length < this.boardSize) {
+              tempCol[o].push("clear");
+            }
+          }
+
+          while (tempCol[o].length < this.boardSize) {
+            tempCol[o].push("clear");
+          }
+        }
+
+        for (let r = 0; r < this.boardSize; r++) {
+          let valid = true;
+          let val = "";
+          for (let o = 0; o < options; o++) {
+            if (val == "") {
+              val = tempCol[o][r];
+            } else if (val != tempCol[o][r]) {
+              valid = false;
+              break;
+            }
+          }
+
+          if (valid) {
+            this.setCell(r, i, tempCol[0][r]);
+          }
+        }
+      }
+    },
+    checkRows() {
+      for (let i = 0; i < this.rows.length; i++) {
+        if (this.rows[i].length == 1 && this.rows[i][0] == this.boardSize) {
+          for (let k = 0; k < this.boardSize; k++) {
+            this.setCell(i, k, "active");
+          }
+        }
+
+        let options =
+          this.boardSize -
+          (this.rows[i].reduce((a, b) => a + b, 0) + this.rows[i].length - 1) +
+          1;
+
+        let tempRow = [];
+        for (let o = 0; o < options; o++) {
+          tempRow[o] = [];
+
+          for (let j = 0; j < o; j++) {
+            tempRow[o].push("clear");
+          }
+
+          for (let k = 0; k < this.rows[i].length; k++) {
+            for (let j = 0; j < this.rows[i][k]; j++) {
+              tempRow[o].push("active");
+            }
+            if (tempRow.length < this.boardSize) {
+              tempRow[o].push("clear");
+            }
+          }
+
+          while (tempRow[o].length < this.boardSize) {
+            tempRow[o].push("clear");
+          }
+        }
+
+        for (let r = 0; r < this.boardSize; r++) {
+          let valid = true;
+          let val = "";
+          for (let o = 0; o < options; o++) {
+            if (val == "") {
+              val = tempRow[o][r];
+            } else if (val != tempRow[o][r]) {
+              valid = false;
+              break;
+            }
+          }
+
+          if (valid) {
+            this.setCell(i, r, tempRow[0][r]);
+          }
+        }
+      }
+    },
+
+    solveBoard() {
+      this.checkCols();
+      this.interactions++;
+      this.checkRows();
+      this.interactions++;
+    },
+  },
+});
+
+app.mount("#nonogram-board");
