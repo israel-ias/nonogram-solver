8b66b06b381bc21a873e9e6468587609fcd0b78f
Israel Silva <israel@iasdigitalgroup.com>
2024-11-08 15:35:56 -0500

    add new implementations

--- src/components/TheSolver.vue
+++ src/components/TheSolver.vue
@@ -1,45 +1,46 @@
 <template>
-  <section id="nonogram-board">
-    <div>
-      <div>
-        <table :key="interactions">
-          <tr class="col-header">
-            <td></td>
-            <td v-for="(col, colIndex) in data.cols" :key="colIndex">
-              <p v-for="(hint, hintIndex) in col" :key="hintIndex">
-                {{ hint }}
-              </p>
-            </td>
-          </tr>
-          <tr v-for="(row, rowIndex) in board" :key="rowIndex">
-            <td class="row-header">
-              <span v-for="(hint, hintIndex) in data.rows[rowIndex]" :key="hintIndex">
-                {{ hint }}
-              </span>
-            </td>
-            <td v-for="(cell, cellIndex) in row" :key="cellIndex" :class="cell"></td>
-          </tr>
-        </table>
-      </div>
-      <button v-on:click="generatePossibilities()">solve</button>
+	<section id="nonogram-board">
+		<div>
+			<div>
+				<table :key="interactions">
+					<tr class="col-header">
+						<td></td>
+						<td v-for="(col, colIndex) in data.cols" :key="colIndex">
+							<p v-for="(hint, hintIndex) in col" :key="hintIndex">
+								{{ hint }}
+							</p>
+						</td>
+					</tr>
+					<tr v-for="(row, rowIndex) in board" :key="rowIndex">
+						<td class="row-header">
+							<span v-for="(hint, hintIndex) in data.rows[rowIndex]" :key="hintIndex">
+								{{ hint }}
+							</span>
+						</td>
+						<td v-for="(cell, cellIndex) in row" :key="cellIndex" :class="cell"></td>
+					</tr>
+				</table>
+			</div>
+			<button v-on:click="solveBoard()">solve</button>
 
-      <br />
-      <br />
-      <p>{{ interactions }} - {{ isValid }} - {{ checkType }} - idx: {{ idx }}</p>
-    </div>
-    <div>
-      <table :key="optIdx">
-        <tr v-for="(row, rowIndex) in board.length" :key="rowIndex">
-          <td v-for="(cell, cellIndex) in board.length" :key="cellIndex" :class="cell"></td>
-        </tr>
-      </table>
-    </div>
-  </section>
+			<br />
+			<br />
+			<p>{{ interactions }} - {{ isValid }} - {{ checkType }} - idx: {{ idx }}</p>
+		</div>
+		<div>
+			<table :key="optIdx">
+				<tr v-for="(row, rowIndex) in board.length" :key="rowIndex">
+					<td v-for="(cell, cellIndex) in board.length" :key="cellIndex" :class="cell"></td>
+				</tr>
+			</table>
+		</div>
+	</section>
 </template>
 
 <script setup lang="ts">
-import data from '../games/data10';
+import data from '../games/data5';
 import { ref, onMounted, type Ref } from 'vue';
+import { generateColOptions, generateRowOptions } from '../helper/solver-handler';
 
 let interactions = ref(0);
 let isValid = ref(false);
@@ -47,250 +48,143 @@ let checkType = ref('');
 let idx = ref(0);
 let board: Ref<string[][]> = ref([]);
 let optIdx = ref(0);
+let rowsOptions: Ref<number[][][]> = ref([]);
+let colsOptions: Ref<number[][][]> = ref([]);
+let isFirstCall = ref(true);
 
 onMounted(() => {
-  board.value = Array.from({ length: data.boardSize }, () =>
-    Array.from({ length: data.boardSize }, () => '')
-  );
+	board.value = Array.from({ length: data.boardSize }, () =>
+		Array.from({ length: data.boardSize }, () => '')
+	);
 });
 
 const setCell = (row: number, col: number, value: string) => {
-  if (board.value[row][col] == '') {
-    board.value[row][col] = value;
-  }
+	if (board.value[row][col] == '') {
+		board.value[row][col] = value;
+	}
 };
 
 const checkCols = () => {
-  for (let i = 0; i < data.boardSize; i++) {
-    let col: string[] = [];
-    for (let j = 0; j < data.boardSize; j++) {
-      col.push(board.value[j][i]);
-    }
-    console.group('checking column', i);
-    col = checkCollection(data.cols[i], col);
-    console.log('returned collection', col);
-    console.groupEnd();
-    for (let j = 0; j < data.boardSize; j++) {
-      board.value[j][i] = col[j];
-    }
-  }
-};
-const generatePossibilities = () => {
-  tests(1, 1);
-  tests(1, 2);
-  tests(1, 3);
-  tests(1, 4);
-  tests(1, 5);
-  tests(2, 2);
-  tests(2, 3);
-  tests(2, 4);
-  tests(3, 3);
-};
-
-const tests = (length: number, cluesSum: number) => {
-  const boardSize = 5;
-  //   const params = [1];
-
-  //   const cluesSum = params.reduce((a, b) => a + b, 0);
-  const numberOfClues = length;
-  const minEmpty = length - 1;
-  const minNeeded = cluesSum + minEmpty;
-  const freeSpaces = boardSize - minNeeded;
-
-  const possibilitiesCount = calculatePossibilities(freeSpaces + numberOfClues, numberOfClues);
-  console.log(
-    `length: ${length}, cluesSum: ${cluesSum}, possibilitiesCount: ${possibilitiesCount}`
-  );
-
-  //     data.boardSize - (params.reduce((a, b) => a + b, 0) + params.length - 1) + 1;
-  //   let possibilities = [];
-  //   let p = 0;
-  //   for (let i = 0; i < possibilitiesCount; i++) {
-  //     let option = [];
-  //     for (let j = 0; j < p; j++) {
-  //       option.push(0);
-  //     }
-
-  //     for (let k = 0; k < params.length; k++) {
-  //       for (let j = 0; j < params[k]; j++) {
-  //         option.push(1);
-  //       }
-  //       if (option.length < boardSize) {
-  //         option.push(0);
-  //       }
-  //     }
-
-  //     for (let i = option.length; i < boardSize; i++) {
-  //       option.push(0);
-  //     }
-
-  //     possibilities.push(option);
-  //   }
-
-  //   console.table(possibilities);
-};
-
-const calculatePossibilities = (N: number, R: number) => {
-  R = Math.min(R, N - R);
-
-  let ans = 1;
-  for (let i = 0; i < R; i++) {
-    ans = ans * (N - i);
-    ans = ans / (i + 1);
-  }
-  return ans;
-
-  // calculation a!/(b!(a-b)!) stars and bars theorem
+	for (let i = 0; i < data.boardSize; i++) {
+		let col: string[] = [];
+		for (let j = 0; j < data.boardSize; j++) {
+			col.push(board.value[j][i]);
+		}
+		console.group('checking column', i);
+		col = checkCollection(data.cols[i], col, i, colsOptions.value);
+		console.log('returned collection', col);
+		console.groupEnd();
+		for (let j = 0; j < data.boardSize; j++) {
+			board.value[j][i] = col[j];
+		}
+	}
 };
 
 const checkRows = () => {
-  for (let i = 0; i < data.boardSize; i++) {
-    console.group('checking row', i);
-    board.value[i] = checkCollection(data.rows[i], board.value[i]);
-    console.log('returned collection', board.value[i]);
-    console.groupEnd();
-  }
+	for (let i = 0; i < data.boardSize; i++) {
+		console.group('checking row', i);
+		board.value[i] = checkCollection(data.rows[i], board.value[i], i, rowsOptions.value);
+		console.log('returned collection', board.value[i]);
+		console.groupEnd();
+	}
 };
 
-const checkCollection = (params: number[], boardCol: string[]) => {
-  console.log('collection to check', params, boardCol);
+const checkCollection = (
+	params: number[],
+	boardCol: string[],
+	idx: number,
+	options: number[][][]
+) => {
+	console.log('collection to check', params, boardCol);
 
-  //check if the collection is already filled
-  if (params.reduce((a, b) => a + b, 0) == boardCol.filter((x) => x == 'active').length) {
-    return boardCol.map((x) => (x == '' ? 'clear' : x));
-  }
+	//check if the collection is already filled
+	if (params.reduce((a, b) => a + b, 0) == boardCol.filter((x) => x == 'active').length) {
+		return boardCol.map((x) => (x == '' ? 'clear' : x));
+	}
 
-  //check if the param is the same size as the board
-  //then set all as active
-  if (params.length == 1 && params[0] == data.boardSize) {
-    return boardCol.map(() => 'active');
-  }
+	//check if the param is the same size as the board
+	//then set all as active
+	if (params.length == 1 && params[0] == data.boardSize) {
+		return boardCol.map(() => 'active');
+	}
 
-  //get the number of possibilities for the collection
-  let possibilities = data.boardSize - (params.reduce((a, b) => a + b, 0) + params.length - 1) + 1;
+	//check if a cell has the same value in all the possibilities
+	for (let r = 0; r < data.boardSize; r++) {
+		let valid = true;
+		let remove = false;
+		const boardVal = boardCol[r] == '' ? -1 : boardCol[r] == 'active' ? 1 : 0;
+		let val = -1;
+		for (let p = 0; p < options.length; p++) {
+			if (val == -1) {
+				val = options[idx][p][r];
+				if (val != boardVal && boardVal != -1) {
+					remove = true;
+					valid = false;
+					break;
+				}
+			} else if (val != options[idx][p][r]) {
+				valid = false;
+				break;
+			}
+		}
 
-  let options = [];
-  for (let p = 0; p < possibilities; p++) {
-    let option = [];
+		if (remove) {
+			options[idx] = options[idx].splice(r, 1);
+		}
 
-    let keepGoing = true;
-    //populate the first cells with clear depending on the
-    //possibility that we are creating
-    for (let j = 0; j < p; j++) {
-      // check if the cell is already active, if so, this is
-      // is not a possibility any more
-      if (boardCol[p] == 'active') {
-        keepGoing = false;
-        break;
-      }
-      option.push('clear');
-    }
+		if (valid) {
+			boardCol[r] = options[idx][0][r] == 1 ? 'active' : 'clear';
+		}
+	}
 
-    if (!keepGoing) {
-      continue;
-    }
-
-    //populate the cells with active depending on the params
-    for (let k = 0; k < params.length; k++) {
-      for (let j = 0; j < params[k]; j++) {
-        // check if the cell is already clear, if so, this is
-        // is not a possibility any more
-        if (boardCol[p] == 'clear') {
-          keepGoing = false;
-          break;
-        }
-        option.push('active');
-      }
-      if (!keepGoing) {
-        break;
-      }
-      if (option.length < data.boardSize) {
-        option.push('clear');
-      }
-    }
-    if (!keepGoing) {
-      continue;
-    }
-
-    //populate the rest of the cells with clear
-    for (let i = option.length; i < data.boardSize; i++) {
-      if (boardCol[i] == 'active') {
-        keepGoing = false;
-        break;
-      }
-      option.push('clear');
-    }
-
-    if (!keepGoing) {
-      continue;
-    }
-
-    //add the possibility to the list of options
-    options.push(option);
-  }
-
-  console.table(options);
-
-  //check if a cell has the same value in all the possibilities
-  for (let r = 0; r < data.boardSize; r++) {
-    let valid = true;
-    let val = '';
-    for (let p = 0; p < options.length; p++) {
-      if (val == '') {
-        val = options[p][r];
-      } else if (val != options[p][r]) {
-        valid = false;
-        break;
-      }
-    }
-
-    if (valid) {
-      boardCol[r] = options[0][r];
-    }
-  }
-
-  return boardCol;
+	return boardCol;
 };
 
 const validateBoard = () => {
-  for (let i = 0; i < data.boardSize; i++) {
-    if (board.value[i].filter((x) => x == '').length > 0) {
-      isValid.value = false;
-      return;
-    }
-  }
-  isValid.value = true;
+	for (let i = 0; i < data.boardSize; i++) {
+		if (board.value[i].filter((x) => x == '').length > 0) {
+			isValid.value = false;
+			return;
+		}
+	}
+	isValid.value = true;
 };
 
 const solveBoard = () => {
-  if (checkType.value == 'rows') {
-    console.group('checking row', idx.value);
-    board.value[idx.value] = checkCollection(data.rows[idx.value], board.value[idx.value]);
-    console.log('returned collection', board.value[idx.value]);
-    console.groupEnd();
-    idx.value++;
-    if (idx.value >= data.boardSize) {
-      checkType.value = 'cols';
-      idx.value = 0;
-    }
-  } else {
-    let col = [];
-    for (let j = 0; j < data.boardSize; j++) {
-      col.push(board.value[j][idx.value]);
-    }
-    console.group('checking column', idx.value);
-    col = checkCollection(data.cols[idx.value], col);
-    console.log('returned collection', col);
-    console.groupEnd();
-    for (let j = 0; j < data.boardSize; j++) {
-      board.value[j][idx.value] = col[j];
-    }
-    idx.value++;
-    if (idx.value >= data.boardSize) {
-      checkType.value = 'rows';
-      idx.value = 0;
-      interactions.value++;
-    }
-  }
+	if (isFirstCall.value) {
+		rowsOptions.value = generateRowOptions(data.rows, data.boardSize);
+		colsOptions.value = generateColOptions(data.cols, data.boardSize);
+		isFirstCall.value = false;
+	}
+
+	if (checkType.value == 'rows') {
+		board.value[idx.value] = checkCollection(
+			data.cols[idx.value],
+			board.value[idx.value],
+			idx.value,
+			rowsOptions.value
+		);
+
+		idx.value++;
+		if (idx.value >= data.boardSize) {
+			checkType.value = 'cols';
+			idx.value = 0;
+		}
+	} else {
+		let col = [];
+		for (let j = 0; j < data.boardSize; j++) {
+			col.push(board.value[j][idx.value]);
+		}
+		col = checkCollection(data.cols[idx.value], col, idx.value, colsOptions.value);
+		for (let j = 0; j < data.boardSize; j++) {
+			board.value[j][idx.value] = col[j];
+		}
+		idx.value++;
+		if (idx.value >= data.boardSize) {
+			checkType.value = 'rows';
+			idx.value = 0;
+			interactions.value++;
+		}
+	}
 };
 </script>
--- src/helper/solver-handler.ts
+++ src/helper/solver-handler.ts
@@ -1,89 +1,110 @@
 const calculatePossibilities = (N: number, R: number) => {
-  R = Math.min(R, N - R);
+	R = Math.min(R, N - R);
 
-  let ans = 1;
-  for (let i = 0; i < R; i++) {
-    ans = ans * (N - i);
-    ans = ans / (i + 1);
-  }
-  return ans;
+	let ans = 1;
+	for (let i = 0; i < R; i++) {
+		ans = ans * (N - i);
+		ans = ans / (i + 1);
+	}
+	return ans;
 
-  // calculation a!/(b!(a-b)!) stars and bars theorem
+	// calculation a!/(b!(a-b)!) stars and bars theorem
 };
 
 const generateOptions = (params: number[], boardSize: number): number[][] => {
-  console.log(params, boardSize);
-  const cluesSum = params.reduce((a, b) => a + b, 0);
-  const numberOfClues = params.length;
-  const minEmpty = params.length - 1;
-  const minNeeded = cluesSum + minEmpty;
-  const freeSpaces = boardSize - minNeeded;
+	console.log(params, boardSize);
+	const cluesSum = params.reduce((a, b) => a + b, 0);
+	const numberOfClues = params.length;
+	const minEmpty = params.length - 1;
+	const minNeeded = cluesSum + minEmpty;
+	const freeSpaces = boardSize - minNeeded;
 
-  let options = [];
-  if (params.length > 2) {
-    const firstParam = params[0];
-    params = params.slice(1);
-    options = generateOptions(params, boardSize - firstParam);
-    const result = [];
-    for (let j = 0; j < firstParam; j++) {
-      result.push(1);
-    }
-    result.push(0);
-    for (let i = 0; i < options.length; i++) {
-      result.push([...options[i]]);
-    }
+	const options = [];
+	if (params.length > 2) {
+		const firstParam = params[0];
+		params = params.slice(1);
 
-    return options;
-  }
+		for (let o = 0; o < freeSpaces; o++) {
+			const result = [];
 
-  const possibilities = calculatePossibilities(freeSpaces + numberOfClues, numberOfClues);
+			for (let j = 0; j < o; j++) {
+				result.push(0);
+			}
 
-  console.log(possibilities);
+			for (let j = 0; j < firstParam; j++) {
+				result.push(1);
+			}
+			result.push(0);
 
-  let spaceBefore = 0;
-  let spaceBetween = 0;
+			const subOptions = generateOptions(params, boardSize - (boardSize - result.length));
 
-  for (let p = 0; p < possibilities; p++) {
-    const option = [];
-    for (let j = 0; j < spaceBefore; j++) {
-      option.push(0);
-    }
+			for (let i = 0; i < subOptions.length; i++) {
+				const tmp = [...result];
+				tmp.push(...subOptions[i]);
+				options.push(tmp);
+			}
+		}
 
-    for (let j = 0; j < params.length; j++) {
-      for (let k = 0; k < params[j]; k++) {
-        option.push(1);
-      }
+		return options;
+	}
 
-      if (option.length < boardSize) {
-        option.push(0);
-      }
+	const possibilities = calculatePossibilities(freeSpaces + numberOfClues, numberOfClues);
 
-      if (j == 0) {
-        for (let i = 0; i < spaceBetween; i++) {
-          option.push(0);
-        }
-      }
-    }
+	let spaceBefore = 0;
+	let spaceBetween = 0;
 
-    for (let i = option.length; i < boardSize; i++) {
-      option.push(0);
-    }
+	for (let p = 0; p < possibilities; p++) {
+		const option = [];
+		for (let j = 0; j < spaceBefore; j++) {
+			option.push(0);
+		}
 
-    options.push(option);
+		for (let j = 0; j < params.length; j++) {
+			for (let k = 0; k < params[j]; k++) {
+				option.push(1);
+			}
 
-    spaceBetween++;
-    if (params.length == 1 || spaceBetween + spaceBefore > freeSpaces) {
-      spaceBetween = 0;
-      spaceBefore++;
-    }
-  }
+			if (option.length < boardSize) {
+				option.push(0);
+			}
 
-  return options;
+			if (j == 0) {
+				for (let i = 0; i < spaceBetween; i++) {
+					option.push(0);
+				}
+			}
+		}
+
+		for (let i = option.length; i < boardSize; i++) {
+			option.push(0);
+		}
+
+		options.push(option);
+
+		spaceBetween++;
+		if (params.length == 1 || spaceBetween + spaceBefore > freeSpaces) {
+			spaceBetween = 0;
+			spaceBefore++;
+		}
+	}
+
+	return options;
 };
 
-const params = [1, 1, 1];
-const boardSize = 10;
+const generateRowOptions = (rowsParams: number[][], boardSize: number): number[][][] => {
+	const rowsPossibilities = [];
+	for (let i = 0; i < boardSize; i++) {
+		rowsPossibilities.push(generateOptions(rowsParams[i], boardSize));
+	}
+	return rowsPossibilities;
+};
 
-const options = generateOptions(params, boardSize);
+const generateColOptions = (colsParams: number[][], boardSize: number): number[][][] => {
+	const colsPossibilities = [];
+	for (let i = 0; i < boardSize; i++) {
+		colsPossibilities.push(generateOptions(colsParams[i], boardSize));
+	}
+	return colsPossibilities;
+};
 
-console.log(options);
+export { generateColOptions, generateRowOptions };
